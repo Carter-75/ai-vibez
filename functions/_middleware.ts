aad42be4d0f5fce55e43baff545bc65064a64fb8
Carter <ph75nix@proton.me>
2025-10-02 22:17:53 -0500

    Delete functions directory

--- functions/_middleware.ts
+++
@@ -1,7 +0,0 @@
-// Cloudflare Pages Functions middleware
-// Basic middleware for Pages Functions
-
-export async function onRequest(context: any) {
-  // Continue to next handler
-  return context.next();
-}
